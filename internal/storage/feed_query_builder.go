572f2b900fad3012938429a724a84c857bbbfd31
grigory <admin@mael.is>
2026-05-12 12:36:23 -0700

    feat: include cleanup_read_days in feed storage queries
    Update CreateFeed, UpdateFeed, and GetFeeds to read and write
    the new nullable cleanup_read_days column.

--- internal/storage/feed_query_builder.go
+++ internal/storage/feed_query_builder.go
@@ -177,7 +177,8 @@ func (f *feedQueryBuilder) GetFeeds() (model.Feeds, error) {
 			f.pushover_enabled,
 			f.pushover_priority,
 			f.proxy_url,
-			f.ignore_entry_updates
+			f.ignore_entry_updates,
+			f.cleanup_read_days
 		FROM
 			feeds f
 		LEFT JOIN
@@ -260,6 +261,7 @@ func (f *feedQueryBuilder) GetFeeds() (model.Feeds, error) {
 			&feed.PushoverPriority,
 			&feed.ProxyURL,
 			&feed.IgnoreEntryUpdates,
+			&feed.CleanupReadDays,
 		)
 
 		if err != nil {
